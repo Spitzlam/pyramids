d1ecad13801e0acf468e118dc364cc8d77385ef2
Michal Sifalda <spitzlam09@gmail.com>
2023-11-29 19:39:46 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    for (int y = 0; y < i; y++)
+    {
+        Console.Write(">");
+    }
+    Console.WriteLine();
+}
+for (int i = 10; i >= 1; i--)
+{
+    for (int y = 0; y < i; y++)
+    {
+        Console.Write("<");
+    }
+    Console.WriteLine();
+}
